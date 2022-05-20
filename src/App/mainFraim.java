342d8b159ae219f6c02b911b3e9cf8f078e58a3f
ShAssaf <shlomo.assaf7@gmail.com>
2022-05-20 17:54:21 +0300

    bu

---
+++ src/App/mainFraim.java
@@ -0,0 +1,14 @@
+package App;
+import Ex3.Task;
+
+public class mainFraim {
+	static Task[] openTasks;
+	public class Main {
+		public static void main(String [] args) {
+			
+		
+		}
+	}
+	public 
+
+}
