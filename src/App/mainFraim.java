92132780acca76270e05d30ea1e78060c104d543
ShAssaf <shlomo.assaf7@gmail.com>
2022-05-20 18:28:35 +0300

    bu

--- src/App/mainFraim.java
+++ src/App/mainFraim.java
@@ -1,45 +1,46 @@
 package App;
+
 import java.util.Scanner;
 import Ex3.*;
-
 
 public class mainFraim {
 	Task[] openTask;
 	static TaskArr openTasks;
 	static Scanner in = new Scanner(System.in);
-	
-	public static void main(String [] args) {
-		//		variable
-		String choice = new String();				//the choice that the user will make
-		
-		//the Program
-		while(choice.compareTo("5") != 0) {		//compareTo return 0 if true
+
+	public static void main(String[] args) {
+		// variable
+		String choice = new String(); // the choice that the user will make
+
+		// the Program
+		while (choice.compareTo("5") != 0) { // compareTo return 0 if true
 			printMenu();
 			choice = in.nextLine();
-			switch(choice) {
-				case("1"):		
-					
-			}
-		in.close();
-		}
-		}
+			switch (choice) {
+			case ("1"):
 
-	
-	public void addTask(Task[] taskList ) {
-		
+			}
+			in.close();
+		}
 	}
-	public void deleteTask() {}
-	public void updateTask() {}
-	public static void  printMenu() {		
+
+	public void addTask(Task[] taskList) {
+
+	}
+
+	public void deleteTask() {
+	}
+
+	public void updateTask() {
+	}
+
+	public static void printMenu() {
 		System.out.println("-----Task Menager------");
 		System.out.println("Option:");
 		System.out.println("1 - Add Task");
 		System.out.println("2 - Delete Task");
 		System.out.println("3 - Modify Task");
-		System.out.println("5 - Exit");}
-
+		System.out.println("5 - Exit");
 	}
-	
-	
 
-
+}
