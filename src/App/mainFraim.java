bc0fa9fa68d54a9d12a200ebc16831b55e7c7348
ShAssaf <shlomo.assaf7@gmail.com>
2022-05-20 18:22:18 +0300

    bu

--- src/App/mainFraim.java
+++ src/App/mainFraim.java
@@ -1,17 +1,44 @@
 package App;
 import java.util.Scanner;
+import Ex3.*;
 
-import Ex3.Task;
 
 public class mainFraim {
-	static Task[] openTasks;
+	Task[] openTask;
+	static Ta openTasks;
 	static Scanner in = new Scanner(System.in);
+	
 	public static void main(String [] args) {
-		System.out.println("sdfds");
+		//		variable
+		String choice = new String();				//the choice that the user will make
+		
+		//the Program
+		while(choice.compareTo("4") != 0) {		//compareTo return 0 if true
+			printMenu();
+			choice = in.nextLine();
+			switch(choice) {
+				case("1"):		//PhoneBook
+					phonebookApp.menu();}
+			}
+		in.close();
+		}
+
+	
+	public void addTask(Task[] taskList ) {
+		
 	}
-	public void addTask() {}
 	public void deleteTask() {}
 	public void updateTask() {}
+	public static void  printMenu() {		
+		System.out.println("-----Task Menager------");
+		System.out.println("Option:");
+		System.out.println("1 - Add Task");
+		System.out.println("2 - Delete Task");
+		System.out.println("3 - Modify Task");
+		System.out.println("5 - Exit");}
+
+	}
+	
 	
 
 }
--- src/Ex3/TaksArr.java
+++ src/Ex3/TaksArr.java
@@ -1,5 +1,24 @@
 package Ex3;
 
 class TaksArr {
-	Task[] task
+	private Task[] taskArr = new Task[1];
+	private int nextEmpty = 0;
+	private int arrSize = 1;
+
+	private boolean isFull() {
+		return (this.nextEmpty == this.arrSize);
+	}
+
+	public void addTask(Task t) {
+		if (isFull()) {
+			Task[] newTaskArr = new Task[arrSize * 2];
+			for (int i = 0; i < arrSize; i++) {
+				newTaskArr[i] = taskArr[i];
+			}
+			taskArr = newTaskArr;
+		}
+		taskArr[nextEmpty] = t;
+		nextEmpty++;
+		arrSize *= 2;
+	}
 }
