997a46ec9d1664e1466b7f8c133867edfe27ba15
arielkes <arielkeslassy@gmail.com>
2022-06-19 18:00:01 +0300

    replaced TaskArr implementation from using arrays to using Java's ArrayList

--- src/Ex3/Main.java
+++ src/Ex3/Main.java
@@ -1,21 +1,21 @@
 package Ex3;
 
-import java.util.ArrayList;
-
 public class Main {
-	public static void main(String [] args) {
-		Contacts c = new Contacts("ari", "055");
-		Task t1 = new Task("1", "1911-11-01 11:11:11");
-		Task t2 = new Task("2", "1922-02-02 22:22:22");
-		ArrayList<Task> al = new ArrayList<Task>();
-		al.add(t1);
-		al.add(t2);
-		
-		AssignedTask at = new AssignedTask("3", "1933-03-03 13:33:33", c);
-		at.task_print();
-		MultiTask mt = new MultiTask("4", "1944-04-04 14:44:44", al);
-		mt.task_print();
-		mt.get_task_by_title("1").task_print();
-		mt.get_task_by_index(1).task_print();
+	public static void main(String[] args) throws InterruptedException {
+		TaskArr tasks = new TaskArr();
+		Task t1 = new Task("drink milk");
+		Task t2 = new Task("buy milk");
+		tasks.addTask(t1);
+		tasks.addTask(t2);
+		t1.setTargetDate("1999-03-14 08:02:00");
+		t2.setTargetDate("1999-03-12 08:02:00");
+		System.out.println(tasks.taskArr.get(0).getTitle());
+		tasks.sort_by_title();
+		System.out.println(tasks.taskArr.get(0).getTitle());
+		tasks.sort_by_date_started();
+		System.out.println(tasks.taskArr.get(0).getTitle());
+		tasks.sort_by_date_target();
+		System.out.println(tasks.taskArr.get(0).getTitle());
+
 	}
 }
--- src/Ex3/TaskArr.java
+++ src/Ex3/TaskArr.java
@@ -1,24 +1,49 @@
 package Ex3;
 
-public class TaskArr {
-	private Task[] taskArr = new Task[1];
-	private int nextEmpty = 0;
-	private int arrSize = 1;
+import java.util.ArrayList;
+import java.util.Comparator;
 
-	private boolean isFull() {
-		return (this.nextEmpty == this.arrSize);
-	}
+public class TaskArr {
+	ArrayList<Task> taskArr = new ArrayList<Task>();
 
 	public void addTask(Task t) {
-		if (isFull()) {
-			Task[] newTaskArr = new Task[arrSize * 2];
-			for (int i = 0; i < arrSize; i++) {
-				newTaskArr[i] = taskArr[i];
-			}
-			taskArr = newTaskArr;
-		}
-		taskArr[nextEmpty] = t;
-		nextEmpty++;
-		arrSize *= 2;
+		this.taskArr.add(t);
+		
+	}
+	public void deleteTask(Task t) { 
+		this.taskArr.remove(t);
+	}
+	
+	public void sort_by_title() { // sort in place
+       this.taskArr.sort(new Comparator<Task>() {
+            @Override
+            public int compare(Task first, Task second)
+            {
+                return first.getTitle().compareTo(second.getTitle());
+            }
+        }
+       );
+	}
+	
+	public void sort_by_date_started() { // sort in place
+		this.taskArr.sort(new Comparator<Task>() {
+            @Override
+            public int compare(Task first, Task second)
+            {
+                return first.getCreateDate().compareTo(second.getCreateDate());
+            }
+        }
+       );
+	}
+	
+	public void sort_by_date_target() { // sort in place
+		this.taskArr.sort(new Comparator<Task>() {
+            @Override
+            public int compare(Task first, Task second)
+            {
+                return first.getTargetDate().compareTo(second.getTargetDate());
+            }
+        }
+       );
 	}
 }
